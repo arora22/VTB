7d68c064b2435fd1b3c8c75ee4c7041daeebd569
romal <romal@>
2016-02-11 15:45:50 +0530

    1st Draft of WebService For User Name Checking

---
+++ WS_UserNameCheck/src/com/vtb/jersey/Utility.java
@@ -0,0 +1,62 @@
+package com.vtb.jersey;
+
+import org.json.simple.JSONObject;
+
+import com.sun.jersey.json.impl.reader.JsonFormatException;
+
+//import org.codehaus.jettison.json.JSONException;
+//import org.codehaus.jettison.json.JSONObject;
+
+
+public class Utility {
+
+	/**
+	 * Null check Method
+	 * 
+	 * @param txt
+	 * @return
+	 */
+	public static boolean isNotNull(String txt) {
+		return txt != null && txt.trim().length() >= 0 ? true : false;
+	}
+
+	/**
+	 * Method to construct JSON
+	 * 
+	 * @param tag
+	 * @param status
+	 * @return
+	 */
+	public static String constructJSON(String tag, boolean status) {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("tag", tag);
+			obj.put("status", new Boolean(status));
+		} catch (JsonFormatException e) {
+			// TODO Auto-generated catch block
+		}
+		return obj.toString();
+	}
+
+	/**
+	 * Method to construct JSON with Error Msg
+	 * 
+	 * @param tag
+	 * @param status
+	 * @param err_msg
+	 * @return
+	 */
+	public static String constructJSON(String tag, boolean status,
+			String err_msg) {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("tag", tag);
+			obj.put("status", new Boolean(status));
+			obj.put("error_msg", err_msg);
+		} catch (JsonFormatException e) {
+			// TODO Auto-generated catch block
+		}
+		return obj.toString();
+	}
+
+}

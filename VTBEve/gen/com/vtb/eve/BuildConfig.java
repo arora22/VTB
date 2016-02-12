82e08877b9da7107ab49615e55114f7fcc2e44f1
romal <romal@>
2016-02-12 15:49:58 +0530

    Initial Commit

---
+++ VTBEve/gen/com/vtb/eve/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vtb.eve;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ VTBEve/src/com/vtb/eve/RegisterActivity.java
@@ -0,0 +1,5 @@
+package com.vtb.eve;
+
+public class RegisterActivity {
+
+}
